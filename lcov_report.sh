0ae395b85f59867290dff4848436f71a387541dd
Alex Hauer <alex.hauer@happyninja.net>
2016-12-28 16:01:01 +0100

    measurements folder

--- lcov_report.sh
+++ lcov_report.sh
@@ -2,4 +2,4 @@ make clean
 make
 ./life 100 < f0.l | sort > f100.l
 lcov -t 'Life' -o life_test.info -c -d .
-genhtml -o lcov_results life_test.info
+genhtml -o measurements/lcov/html life_test.info
