a4dd05f054c3b8e4180220a8ca60940cee1869d0
Alex Hauer <alex.hauer@happyninja.net>
2016-12-30 12:51:12 +0100

    tag based report generation

--- create_reports.sh
+++ create_reports.sh
@@ -1,47 +1,92 @@
-#!/bin/sh
+#!/bin/bash
 
 lcov_report() {
     TITLE=$1
+    TAG=$2
 
-    echo "lcov report ($TITLE) ..."
-    lcov -t $TITLE -o measurements/lcov/life_test.info -c -d .
-    genhtml -o measurements/lcov/html measurements/lcov/life_test.info
+    printf "lcov report ($TITLE) ...\n"
+    mkdir measurements/lcov/${TAG}
+    lcov -t $TITLE -o measurements/lcov/${TAG}/life_test.info -c -d .
+    genhtml -o measurements/lcov/${TAG}/html measurements/lcov/${TAG}/life_test.info
 }
 
 perf_report() {
     GENERATIONS=$1
     FILE=$2
+    TAG=$3
 
-    echo "perf report for $FILE with $GENERATIONS generations ..."
-    (perf stat -e cycles ./life $GENERATIONS < $FILE | sort > /dev/null) > measurements/perf/${FILE}_${GENERATIONS}.log 2>&1
+    printf "perf report for $FILE with $GENERATIONS generations ...\n"
+    mkdir measurements/perf/${TAG}
+    (perf stat -e cycles ./life $GENERATIONS < $FILE | sort > /dev/null) > measurements/perf/${TAG}/${FILE}_${GENERATIONS}.log 2>&1
 }
 
 oprofile_report() {
     GENERATIONS=$1
     FILE=$2
+    TAG=$3
 
-    echo "oprofile report for $FILE with $GENERATIONS generations ..."
-    mkdir measurements/oprofile/${FILE}_${GENERATIONS}
-    operf --session-dir ./measurements/oprofile/${FILE}_${GENERATIONS} ./life $GENERATIONS < $FILE | sort > /dev/null
-    opreport --session-dir ./measurements/oprofile/${FILE}_${GENERATIONS} > ./measurements/oprofile/${FILE}_${GENERATIONS}/report.log
+    printf "oprofile report for $FILE with $GENERATIONS generations ...\n"
+    mkdir measurements/oprofile/${TAG}/${FILE}_${GENERATIONS}
+    operf --session-dir ./measurements/oprofile/${TAG}/${FILE}_${GENERATIONS} ./life $GENERATIONS < $FILE | sort > /dev/null
+    opreport --session-dir ./measurements/oprofile/${TAG}/${FILE}_${GENERATIONS} > ./measurements/oprofile/${TAG}/${FILE}_${GENERATIONS}/report.log
+}
+
+reports() {
+    GENERATIONS=$1
+    TAG=$2
+
+    FILES=*.l
+    for f in $FILES
+    do
+        echo "test"
+        #perf_report $GENERATIONS $f $TAG
+        #oprofile_report $GENERATIONS $f $TAG
+    done
+}
+
+print_heading() {
+    TEXT="$1"
+    LENGTH=$((${#TEXT} + 2))
+    BORDER_TOP_BOTTOM="-"
+    BORDER_LEFT_RIGHT="|"
+
+    printf -vch "%${LENGTH}s" ""
+    printf "+%s+\n" "${ch// /${BORDER_TOP_BOTTOM}}"
+
+    printf "${BORDER_LEFT_RIGHT} %s ${BORDER_LEFT_RIGHT}\n" "$TEXT"
+
+    printf "+%s+\n" "${ch// /${BORDER_TOP_BOTTOM}}"
 }
 
 #### Main
-echo "Cleaning up previous builds ..."
-make clean
-echo "Building ..."
-make coverage
+print_heading "Report Generation"
 
-FILES=*.l
-for f in $FILES
+TAGS=`git tag`
+for tag in $TAGS
 do
-    perf_report $1 $f
-    #oprofile_report $1 $f
+    print_heading "Generating reports for $tag ..."
+
+    # switch to tag
+    #git checkout -b ${tag}_report $tag
+
+    # clean up and prepare for coverage
+    printf "Cleaning up previous builds ...\n"
+    make clean
+    printf "Building ...\n"
+    make coverage
+
+    # generate reports
+
+    #reports $1 $tag
+    #lcov_report $tag $tag
+
+    # merge reports into master and delete report branch
+    #git checkout master
+    #git merge ${tag}_report --strategy-option theirs
+    #git branch -d ${tag}_report
 done
 
-echo "Generating lcov report ..."
-lcov_report LifeHash
-echo "Done."
-
-echo "Cleaning up ..."
+print_heading "Cleaning up"
 make clean
+
+exit
